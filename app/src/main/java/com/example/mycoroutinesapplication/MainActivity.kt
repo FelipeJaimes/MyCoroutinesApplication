b5a9d3ed0b6e82e2967d6bcee4f3d5a61b797324
Felipe <felipejg8611@gmail.com>
2021-09-29 17:25:33 -0500

    Add basing login with no threading

--- app/src/main/java/com/example/mycoroutinesapplication/MainActivity.kt
+++ app/src/main/java/com/example/mycoroutinesapplication/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.mycoroutinesapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        submitButton.setOnClickListener {
+            val success = validateLogin(userEditText.text.toString(), passwordEditText.text.toString())
+            toast(if (success) "Success" else "Failure")
+        }
+    }
+
+    private fun validateLogin(username: String, password: String): Boolean {
+
+        return username.isNotEmpty() && password.isNotEmpty()
+    }
+
+    private fun Context.toast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
 }
