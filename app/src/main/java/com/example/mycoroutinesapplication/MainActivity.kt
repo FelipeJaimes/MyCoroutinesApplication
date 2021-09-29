c6b9d507ea8133d7d10603af963ad40edc54a26c
Felipe <felipejg8611@gmail.com>
2021-09-29 18:03:27 -0500

    add first mock coroutine

--- app/src/main/java/com/example/mycoroutinesapplication/MainActivity.kt
+++ app/src/main/java/com/example/mycoroutinesapplication/MainActivity.kt
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         submitButton.setOnClickListener {
-            val success = validateLogin(userEditText.text.toString(), passwordEditText.text.toString())
-            toast(if (success) "Success" else "Failure")
+            GlobalScope.launch (Dispatchers.Main) {
+                val success = withContext(Dispatchers.IO){
+                    validateLogin(userEditText.text.toString(), passwordEditText.text.toString())
+                }
+                toast(if (success) "Success" else "Failure")
+            }
         }
     }
 
     private fun validateLogin(username: String, password: String): Boolean {
+        Thread.sleep(2000)
         return username.isNotEmpty() && password.isNotEmpty()
     }
 
